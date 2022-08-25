554e12675ef91b0eb894991a770e6f5b22b68687
mayadagan14 <mayadd1405@gmail.com>
2022-08-25 15:52:20 +0300

    Add project files.

---
+++ Union_Linked_Lists/Source.c
@@ -0,0 +1,81 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "md5.h" //using void md5String(const char* input, char* output);
+#include "md4.h" //using void md4String(const char* input, char* output);
+
+#define MD4_HASH 0
+#define MD5_HASH 1
+#define True 1
+#define False 0
+typedef unsigned char HashType;
+typedef unsigned char Byte;
+typedef char Bool;
+
+typedef struct msgWithHash
+{
+	unsigned int id;
+	char* text;
+	union
+	{
+		Byte md4[16];
+		Byte md5[16];
+	} hashDigest;
+	HashType type;
+}msgWithHash;
+
+typedef struct msgListNode msgListNode;
+struct msgListNode
+{
+	msgWithHash* data;
+	msgListNode* next;
+};
+
+void print_hash(const unsigned char* p);
+msgWithHash* createMessage(unsigned int id, const char* text, HashType type);
+msgWithHash*
+void printMessage(const msgWithHash* message);
+msgListNode* addMessage(msgListNode* head, msgWithHash* data);
+int findMsgByHashIterative(const msgListNode* head, Byte hash[16]);
+int findMsgByHashRecursive(const msgListNode* head, Byte hash[16]);
+msgListNode* deleteMessage(msgListNode* head, unsigned int id);
+Bool verifyIntegrity(msgListNode* head, unsigned int id, Byte compareHash[16]);
+msgListNode* reverseMsgList(msgListNode* head);
+void freeMsgList(msgListNode* head);
+
+int main() {
+	
+	/******************** Example 1 from PDF ********************************************/
+	
+	/*const char* sentence = "this is some text that I want to hash.";
+	printf("text: \"%s\"\n", sentence);
+	unsigned char hash[16] = { 0 };
+	md5String(sentence, hash);
+	printf("md5: ");
+	print_hash(hash);
+
+	printf("md4: ");
+	md4String(sentence, hash);
+	print_hash(hash);*/
+	
+	/******************** Example 2 from PDF ********************************************/
+
+	/*const msgWithHash* msg1 = createMessage(87651233, "", MD4_HASH);
+	const msgWithHash* msg2 = createMessage(3, "banana1", MD5_HASH);
+	const msgWithHash* msg3 = createMessage(275, "banana2!", MD4_HASH);
+	const msgWithHash* msg4 = createMessage(342234, "Hello World", MD4_HASH);
+
+	printMessage(msg1);
+	printMessage(msg2);
+	printMessage(msg3);
+	printMessage(msg4);*/
+
+}
+
+void print_hash(const unsigned char* p) {
+	for (unsigned int i = 0; i < 16; ++i) {
+		printf("%02x", p[i]);
+	}
+	printf("\n");
+}
